cd93e2ed3d03a9aca80400c550f6f84cac797ac0
EdsonMello-code <edsonmelo754@gmail.com>
2021-05-09 16:03:00 -0300

    .

--- sw.js
+++ sw.js
@@ -4,11 +4,10 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
-        "/",
+        ".",
         "./index.html",
-        "./styles.css",
         "./main.js",
-        "./manifest.js",
+        "./manifest.json",
       ]);
     })
   );
